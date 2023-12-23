c626582005d946a1c06e25064d5757e0b010083f
lolepp <rauch-philipp@gmx.de>
2023-12-23 22:14:38 +0100

    changed and modalized computer guess

--- computer_guess.py
+++ computer_guess.py
@@ -1,44 +1,50 @@
 import random
 import time
 
-def game():
-    print("Welcome to my guessing game!\nYou set a number and your computer will try to guess it.\n")
-    
+def get_user_number():
     # User number
     user_number = input("Think about your number that your computer needs to guess (Just don't let it be over 2^63 - 1): ")
     while not user_number.isdigit():
-        print("Again, please answer correctly with a number.")
+        print("Please enter a valid number.")
         user_number = input("Think about your number: ")
-    user_number = int(user_number)
+    return int(user_number)
+
+def init_game(user_number):
     expo = len(str(user_number))
     high = 10 ** expo
     low = 0
-    counter = 0
+    return low, high
 
+def computer_guess(user_number, low, high):
+    counter = 0
     # Random number
+    print(f"Now the computer tries to guess a number between {low} and {high}.\n")
     random_number = random.randint(low, high)
-    print(f"Computers guess: {random_number}")
+    print(f"Computer's guess: {random_number}")
     print(f"Current counter: {counter}")
-    
     # Computer tries to guess the number
     while random_number != user_number:
         if random_number > user_number: # Computer guessed too high
-            print("The computer guessed too high. It will guess lower.")
+            print("The computer guessed too high. It will guess lower.\n")
             high = random_number - 1
         elif random_number < user_number: # Computer guessed too low
-            print("The computer guessed too low. It will guess higher.")
+            print("The computer guessed too low. It will guess higher.\n")
             low = random_number + 1
+        
         # Generating new random number
         random_number = random.randint(low, high)
-        
         counter += 1
-        print(f"Computers guess: {random_number}")
+
+        print(f"Computer's guess: {random_number}")
         print(f"Current counter: {counter}")
-        time.sleep(0.1)
-    print(f"The number {random_number} was correctly guessed!\nFinished!")
+        time.sleep(0.25)
+    print(f"The number {random_number} was correctly guessed!\nFinished.")
 
 def main():
-    game()
+    print("Welcome to my guessing game!\nYou set a number, and your computer will try to guess it.\n")
+    user_number = get_user_number()
+    low, high = init_game(user_number)
+    computer_guess(user_number, low, high)
 
 if __name__ == "__main__":
     main()
