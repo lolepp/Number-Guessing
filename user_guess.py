b1c99a8765f368f4de8d8f6e5df52beede5d650b
lolepp <rauch-philipp@gmx.de>
2023-12-23 18:27:22 +0100

    User guess script refined, now working

--- user_guess.py
+++ user_guess.py
@@ -2,60 +2,66 @@ import random
 import time
 import sys
 
-# def checkNumber(number):
-    
-#     print()
-
-# def userNumber():
-    
-#     print()
-
 def range():
-    range_q = input("First of all, do you want to set the range of the numbers you need to guess. y for yes, n for no (will default to 0 and 100), e to exit (y/n/e/...) ")
+    print("First of all, do you want to set the range in which the number is generated that you will need to guess?")
+    range_q = input("Use y for yes, n for no (will default to 0 and 100) or e to exit (y/n/e): ")
     while True:
-        if range_q == 'y' or 'Y':
-            range_low = input("The lowest range number. ")
-            range_top = input("The highest range number. ")
-            while not range_low.isdigit() and not range_top.isdigit:
-                range_low = input("Again, please answer correctly, with a number.\nThe lowest range number. ")
-                range_top = input("Again, please answer correctly, with a number.\nThe highest range number. ")
-            return (range_low, range_top)
-        elif range_q == 'n' or 'N':
-            return 0, 100
-        elif range_q == 'e' or 'E':
+        if range_q == "y" or range_q == "Y":
+            rangeLow = input("The lowest range number: ")
+            while not rangeLow.isdigit():
+                print("Again, please answer correctly with a number.")
+                rangeLow = input("The lowest range number: ")
+
+            rangeTop = input("The highest range number: ")
+            while not rangeTop.isdigit():
+                print("Again, please answer correctly with a number.")
+                rangeTop = input("The highest range number: ")
+            return [rangeLow, rangeTop]
+        elif range_q == "n" or range_q == "N":
+            return [0, 100]
+        elif range_q == "e" or range_q == "E":
             sys.exit()
         else:
-            range_q = input("Again, please answer correctly, with a number\n. Use y or n. (y/n) ")
+            print("Again, please answer correctly.")
+            range_q = input("Use y for yes, n for no (will default to 0 and 100) or e to exit. (y/n/e): ")
 
 def game(range_low, range_top):
-    print("Welcome to my guessing game.\nThe computer generates a random number and you will try to guess it\nIf you end prematurely, just type anything unwanted.\nGood luck!")
+    print(f"The number that you need to guess is generated in between {range_low} and {range_top}.\n")
+    # User counter
     user_counter = 1
+
     # User number
-    user_number = int(input(f"Your {user_counter}. number here: ")) % range_top
+    user_number = input(f"Your {user_counter}. number here: ")
+    while not user_number.isdigit():
+        print("Again, please answer correctly with a number.")
+        user_number = input(f"Your {user_counter}. number here: ")
+    user_number = int(user_number)
+
     # Random number from computer
-    random_number = random.randint(range_low, range_top)
+    computer_number = random.randint(range_low, range_top)
+    
     # Counter
     counter = 0
     print(f"Current counter: {counter}")
+    
     # Main entry loop
-    while user_number != random_number:
-        if user_number > random_number: # You guessed too high
+    while user_number != computer_number:
+        if user_number > computer_number: # You guessed too high
             print("You guessed too high. Guess lower.")
-            # User number
-            user_number = int(input(f"Your {user_counter}. number here: ")) % range_top
         else: # You guessed too low
             print("You guessed too low. Guess higher.")
-            # User number
-            user_number = int(input(f"Your {user_counter}. number here: ")) % range_top
+        # User number
+        user_number = int(input(f"Enter your {user_counter}. number here: ")) % (range_top + 1)
         user_counter += 1
         counter += 1
         print(f"Current counter: {counter}")
         time.sleep(0.1)
-    print("Correctly guessed!\nFinished!")
+    print(f"You correctly guessed the number {computer_number}!\nFinished!")
 
 def main():
-    x, y = range()
-    game(x, y)
+    print("Welcome to my guessing game.\nThe computer generates a random number and you will try to guess it.\nIf you end prematurely, just type anything unwanted.\nGood luck!\n")
+    ranges = range()
+    game(ranges[0], ranges[1])
 
 if __name__ == "__main__":
     main()
